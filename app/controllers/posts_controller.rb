1b7959a31d2f2b7c55400450d8d5841fa475691f
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-10-17 16:46:06 -0300

    feat: implement responsive layout adjustments and content creation with HTML + Tailwind

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :img_url, :styles_css, :body, :published_at)
+      params.require(:post).permit(:title, :img_url, :body, :published_at)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_173441) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_17_185738) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", limit: 150
     t.text "body"
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_16_173441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url"
-    t.text "styles_css"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
