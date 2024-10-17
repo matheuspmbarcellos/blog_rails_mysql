3075ec7bb7771813ad362a70dec60920976ee075
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-10-17 14:55:22 -0300

    feat: enhance admin area with Tailwind styling and improved UI components

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: { message: "O título não pode ficar em branco" }
+  validates :img_url, presence: { message: "A URL da imagem não pode ficar em branco" }
+  validates :styles_css, presence: { message: "O campo de estilos CSS não pode ficar em branco" }
+  validates :body, presence: { message: "O corpo do post não pode ficar em branco" }
+  validates :published_at, presence: { message: "A data de publicação não pode ficar em branco" }
 end
